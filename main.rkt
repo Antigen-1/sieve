4d6dc939444c19011d5d218915e1b7dd96425dee
Antigen-1 <zhanghao@antigen.top>
2023-09-28 21:50:52 +0800

    refactor and generalize the code

--- main.rkt
+++ main.rkt
@@ -28,32 +28,76 @@
 (require racket/stream (for-syntax racket/base racket/syntax syntax/parse))
 (provide define-sieve-box-constructor)
 
+;;Utilities
+(begin-for-syntax
+  (define ((make-continuations-wrapper pairs) core)
+    ;;pair: (cons operator body)
+    (foldl (lambda (p i) (list (car p) (cdr p) i)) core pairs)))
+
+;;Transformers
+(define-syntax operator/c #'(-> (-> any/c any) stream? stream?))
 (define-syntax (define-sieve-box-constructor stx)
+  (define-splicing-syntax-class pass
+    #:description "Pass in the sieve box"
+    (pattern (~seq operator (body:expr ...))
+             #:declare operator (expr/c #'operator/c)))
   (define-syntax-class sieve-box-constructor
     #:description "Sieve box constructor"
-    (pattern (_ name:id body:expr ...)))
+    (pattern (_ name:id pass:pass ...)
+             #:with (operator ...) #'(pass.operator ...)
+             #:with ((body ...) ...) #'((pass.body ...) ...)))
   
   (syntax-parse stx
     (form:sieve-box-constructor
      (with-syntax
-         ;;Add the scope introduced when the syntax object is passed into the transformer
-         ((stm (cadr (syntax-e (syntax-local-introduce (datum->syntax #f (list #'form (generate-temporary 'stream))))))))
-       #`(define (form.name stm)
-           (let* #,(syntax-local-introduce
-                    ;;Identifiers `current-value` and `other-item` are both available in the body of the filter
-                    #'((current-value (stream-first stm)) (new-filter (lambda (other-item) form.body ...))))
-             (stream-cons #:eager current-value
-                          (form.name (stream-filter new-filter (stream-rest stm))))))))))
+         ((stm (cadr (syntax-e (syntax-local-introduce (datum->syntax #f (list #'form (generate-temporary 'stream))))))) ;;Add the scope introduced when the syntax object is passed into the transformer
+          ((pass ...) (map (lambda _ (generate-temporary #'pass)) (syntax->list #'(form.operator ...))))) ;;Generate names for passes
+       (let ((wrapper (make-continuations-wrapper (map cons (syntax->list #'(form.operator ...)) (syntax->list #'(pass ...))))))
+         #`(define (form.name stm)
+             (let* #,(syntax-local-introduce
+                      ;;Identifiers `CURRENT` and `OTHER` are both available in the body of the filter
+                      ;;Other identifiers are either unstable or inavailable
+                      #'((CURRENT (stream-first stm))
+                         (pass (lambda (OTHER) form.body ...))
+                         ...))
+               (stream-cons #:eager CURRENT
+                            (form.name #,(wrapper #'(stream-rest stm)))))))))))
 
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
   ;; or with `raco test`. The code here does not run when this file is
   ;; required by another module.
 
-  (struct integer (n)
-    #:methods gen:stream [(define (stream-empty? _) #f)
-                          (define (stream-first i) (integer-n i))
-                          (define (stream-rest i) (integer (add1 (integer-n i))))])
-  (define-sieve-box-constructor primes-in
-    (not (zero? (remainder other-item current-value))))
-  (check-true (= 233 (time (stream-ref (primes-in (integer 2)) 50)))))
+  (require racket/math)
+
+  (define (standard-ith-prime i)
+    (define (prime? n)
+      (for/and ((f (in-naturals 2))) #:break (> (* f f) n)
+        (not (zero? (remainder n f)))))
+
+    (let loop ((c 0) (s 2))
+      (cond ((= i c) (sub1 s))
+            ((prime? s) (loop (add1 c) (add1 s)))
+            (else (loop c (add1 s))))))
+
+  (define (new-ith-prime i)
+    (struct integer (n)
+      #:methods gen:stream [(define (stream-empty? _) #f)
+                            (define (stream-first i) (integer-n i))
+                            (define (stream-rest i) (integer (add1 (integer-n i))))])
+    
+    (define-sieve-box-constructor primes-in
+      stream-filter
+      ((not (zero? (remainder OTHER CURRENT)))))
+    
+    (stream-ref (primes-in (integer 2)) (sub1 i)))
+  
+  (check-true (= 233 (standard-ith-prime 51) (new-ith-prime 51)))
+
+  (define i (random 1000 2000))
+  (displayln (format "Standard method: ~a" i))
+  (define result1 (time (standard-ith-prime i)))
+  (displayln (format "Sieve's approach: ~a" i))
+  (define result2 (time (new-ith-prime i)))
+
+  (check-true (= result1 result2)))
