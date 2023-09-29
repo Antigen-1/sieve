c0cd9608a70e7c4b320f2c8b50aeeffb33b48189
Antigen-1 <zhanghao@antigen.top>
2023-09-29 18:53:55 +0800

    refactor the code to improve the performance

--- main.rkt
+++ main.rkt
@@ -25,7 +25,7 @@
 
 ;; Code here
 
-(require racket/stream (for-syntax racket/base racket/syntax syntax/parse))
+(require racket/stream (submod racket/performance-hint begin-encourage-inline) (for-syntax racket/base racket/syntax syntax/parse))
 (provide define-sieve-box-constructor)
 
 ;;Utilities
@@ -39,29 +39,38 @@
 (define-syntax (define-sieve-box-constructor stx)
   (define-splicing-syntax-class pass
     #:description "Pass in the sieve box"
-    (pattern (~seq operator (body:expr ...))
+    (pattern (~seq #:filter (body:expr ...))
+             #:with operator #'stream-filter)
+    (pattern (~seq #:map (body:expr ...))
+             #:with operator #'stream-map)
+    (pattern (~seq #:other operator (body:expr ...))
              #:declare operator (expr/c #'operator/c)))
   (define-syntax-class sieve-box-constructor
     #:description "Sieve box constructor"
     (pattern (_ name:id pass:pass ...)
              #:with (operator ...) #'(pass.operator ...)
              #:with ((body ...) ...) #'((pass.body ...) ...)))
+
+  (define new-macro-intro-scope (make-syntax-introducer #f))
+  (define new-use-site-scope (make-syntax-introducer #t))
   
   (syntax-parse stx
     (form:sieve-box-constructor
      (with-syntax
-         ((stm (cadr (syntax-e (syntax-local-introduce (datum->syntax #f (list #'form (generate-temporary 'stream))))))) ;;Add the scope introduced when the syntax object is passed into the transformer
-          ((pass ...) (map (lambda _ (generate-temporary #'pass)) (syntax->list #'(form.operator ...))))) ;;Generate names for passes
+         ((stm (new-macro-intro-scope (generate-temporary #'stream) 'add)) ;;Add the new macro-introduction scope to the formal parameter
+          (name (new-use-site-scope (datum->syntax #f (syntax->datum #'form.name)) 'add)) ;;Remove the macro-introduction scope and add the new use-site scope
+          ((pass ...) #'((lambda ($OTHER) form.body ...) ...)))
        (let ((wrapper (make-continuations-wrapper (map cons (syntax->list #'(form.operator ...)) (syntax->list #'(pass ...))))))
-         #`(define (form.name stm)
-             (let* #,(syntax-local-introduce
-                      ;;Identifiers `CURRENT` and `OTHER` are both available in the body of the function
-                      ;;Other identifiers are either unstable or inavailable
-                      #'((CURRENT (stream-first stm))
-                         (pass (lambda (OTHER) form.body ...))
-                         ...))
-               (stream-cons #:eager CURRENT
-                            (form.name #,(wrapper #'(stream-rest stm)))))))))))
+         (new-use-site-scope
+          (syntax-local-introduce
+           #`(begin-encourage-inline
+               (define (name stm)
+                 ;;Identifiers `$CURRENT` and `$OTHER` are both available in the body of these functions
+                 ;;Other identifiers are either unstable or inavailable
+                 (let (($CURRENT (stream-first stm)))
+                   (stream-cons #:eager $CURRENT
+                                (name #,(wrapper #'(stream-rest stm))))))))
+          'flip))))))
 
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
@@ -80,17 +89,20 @@
 
   (define (sieve-ith-prime i)
     (define-sieve-box-constructor primes-in
-      stream-filter
-      ((not (zero? (remainder OTHER CURRENT)))))
+      #:filter
+      ((or
+        (> (* $CURRENT $CURRENT) $OTHER)
+        (not (zero? (remainder $OTHER $CURRENT))))))
     
     (stream-ref (primes-in (in-naturals 2)) (sub1 i)))
   
   (check-true (= 233 (standard-ith-prime 51) (sieve-ith-prime 51)))
 
+  ;;Currently, the sieving approach is still much slower than the standard method
   (define i (random 1000 2000))
   (displayln (format "Standard method: ~a" i))
   (define result1 (time (standard-ith-prime i)))
-  (displayln (format "Sieve's approach: ~a" i))
+  (displayln (format "Sieving approach: ~a" i))
   (define result2 (time (sieve-ith-prime i)))
 
   (check-true (= result1 result2)))
